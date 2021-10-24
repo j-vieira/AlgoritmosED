9affb19d554f1a08665ff940ac10a1c5f6533a78
unknown <jvsantospessoal@gmail.com>
2021-10-23 22:32:32 -0300

    Transformando horas e minutos para segundo, exercicio13

---
+++ python/lista1/exercicio13.py
@@ -0,0 +1,10 @@
+totalHoras = int(input("Coloque a quantidade de horas: "))
+totalMinutos = int(input("Coloque a quantidade de minutos: "))
+totalSegundos = int(input("Coloque a quantidade de segundos: "))
+
+horasEmSegundos = totalHoras*60*60
+minutosEmSegundos = totalMinutos*60
+
+totalSegundos = totalSegundos + horasEmSegundos + minutosEmSegundos 
+
+print(totalSegundos)
