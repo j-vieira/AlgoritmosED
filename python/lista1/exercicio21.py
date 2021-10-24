abdda9bb01ddf29be50e80805a539e8f43275318
unknown <jvsantospessoal@gmail.com>
2021-10-24 11:32:00 -0300

    Exercicio 21, calculadora de quantos salarios minimos um funcionario ganha.

---
+++ python/lista1/exercicio21.py
@@ -0,0 +1,5 @@
+salarioFuncionario = float(input("Coloque o salario do funcionario: "))
+salarioMinimoVigente = 1192.40
+quantidadeSalarioMinimo = salarioFuncionario//salarioMinimoVigente
+
+print("O funcionario ganha " + str(quantidadeSalarioMinimo) + " salarios minimos.")
