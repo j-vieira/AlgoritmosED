6e5cee20a6e1b85e7a06b398ef3ab7e757768059
unknown <jvsantospessoal@gmail.com>
2021-10-27 21:46:49 -0300

    Adicionando algoritmo que imprime 3 numeros do usuario em ordem crescente implementado com selection sort

---
+++ python/lista2/exercicio1.py
@@ -0,0 +1,20 @@
+numero1 = int(input("Coloque o primeiro numero: "))
+numero2 = int(input("Coloque o segundo numero: "))
+numero3 = int(input("Coloque o terceiro numero: "))
+
+numeros = [numero1, numero2, numero3]
+
+print("A lista de numeros inicialmente é:", numeros)
+
+def ordenar(numeros):
+    for i in range(1, len(numeros)):
+        chave = numeros[i]
+        k = i
+        while k > 0 and chave < numeros[k-1]:
+            numeros[k] = numeros[k-1]
+            k = k-1
+        numeros[k] = chave
+
+ordenar(numeros)
+
+print("Estes são os numeros de forma crescente:", numeros)
