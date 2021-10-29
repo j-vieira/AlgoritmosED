b048bae6d03dc091125a8de4430d520237fba476
unknown <jvsantospessoal@gmail.com>
2021-10-28 21:42:04 -0300

    Calculadora de qual professor ganhou mais

---
+++ python/lista2/exercicio7.py
@@ -0,0 +1,7 @@
+quantidadeDeHorasAulas1 = int(input("Quantidade de horas dadas de aula pelo professor Sena: "))
+quantidadeDeHorasAulas2 = int(input("Quantidade de horas dadas de aula pelo professor Behague: "))
+
+if(quantidadeDeHorasAulas1 > quantidadeDeHorasAulas2):
+    print("O professor Sena ganhou mais do que o Behague.")
+else: 
+    print("O professor Behague ganhou mais do que o Sena.")
