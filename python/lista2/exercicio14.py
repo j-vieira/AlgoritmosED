7a3eb4c976862c57788b7e86f1002a78d2f815a5
unknown <jvsantospessoal@gmail.com>
2021-11-01 16:57:37 -0300

    Exercicio 14, enunciado mal feito

---
+++ python/lista2/exercicio14.py
@@ -0,0 +1,8 @@
+#Considerando uma hora de viagem
+totalHoras = int(input("Coloque a quantidade de horas: "))
+totalMinutos = int(input("Coloque a quantidade de minutos: "))
+totalSegundos = int(input("Coloque a quantidade de segundos: "))
+
+print(str(totalHoras) + " horas " + str(totalMinutos) + " minutos " + str(totalSegundos) + " segundos é a hora de partida")
+
+print(str(totalHoras+1) + " horas " + str(totalMinutos) + " minutos " + str(totalSegundos) + " segundos é a hora de chegada")
